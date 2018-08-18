5196d97b805e68a2e82fde7149e6f426346ee2f6
lakshmithirupathammapathi <41730938+lakshmithirupathammapathi@users.noreply.github.com>
2018-08-18 10:19:57 +0530

    Create guvi54.java

---
+++ guvi54.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=123;
+		if(n%2==0)
+		{
+			System.out.println("the number is even");
+		}
+		else
+		{
+		n=n-1;
+		System.out.println("the next number is"+n);
+
+}
+		}
+		}
